69608eb9bbdcad5e37a0331eac0f9cf47b38486c
Borodin Dmitriy <dmitriy@borodin.site>
2018-07-02 18:37:56 +0300

    [ HOT_FIX ]

--- src/utils/yearOnly.ts
+++ src/utils/yearOnly.ts
@@ -2,5 +2,5 @@ import { Moment } from "moment";
 import * as moment from "moment";
 
 export function yearOnly(date: Moment): Moment {
-  return moment(`${date.year()}-01-01`).utc();
+  return moment(`${date.year()}-01-01`).utc(true);
 }
