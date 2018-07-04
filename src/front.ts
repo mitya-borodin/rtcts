11a5ef8f3eee2f8fde8c5037fd9d345225f02d3c
Borodin Dmitriy <dmitriy@borodin.site>
2018-07-04 16:22:43 +0300

    [ ADD ]

--- src/front.ts
+++ src/front.ts
@@ -3,3 +3,4 @@ export { UserService } from "./front/UserService";
 export { Service } from "./front/Service";
 export { WSClient } from "./front/WSClient";
 export { RepositoryStore } from "./front/RepositoryStore";
+export { Store } from "./front/Store";
