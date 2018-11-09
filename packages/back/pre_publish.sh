0dea3c7da0e49dc93c766b168233f6019eb19893
Дмитрий Бородин <dmitriy@borodin.site>
2018-11-09 22:29:53 +0300

    1. пересборка и линковка.

---
+++ packages/back/pre_publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cwd=$(pwd)
+tgdir=$cwd/pre_publish_build_tmp
+
+
+mkdir $tgdir
+cd $tgdir
+
+git clone $cwd .
+
+npm i
+npm run lint
+npm run test
+npm run build
+
+cd ..
+rm -rf $tgdir
+
+echo "Pre publish test well done;"
+
+exit 0
--- re_build.sh
+++ re_build.sh
@@ -3,19 +3,24 @@ cwd=$(pwd)
 
 cd $cwd/packages/vendors-dll
 npm run build
+npm link
 
 cd ../interfaces
 npm run build
+npm link
 
 cd ../utils
 npm run build
+npm link
 
 cd ../isomorphic
 npm run build
+npm link
 
 cd ../front
 npm run build
+npm link
 
 cd ../back
 npm run build
-
+npm link
