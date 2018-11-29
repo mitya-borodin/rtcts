acd09485fb95450e79894d44bd843570fcc9847d
Дмитрий Бородин <dmitriy@borodin.site>
2018-11-29 16:47:22 +0300

    1. hot_fix

--- packages/utils/src/checkPassword.ts
+++ packages/utils/src/checkPassword.ts
@@ -1,5 +1,5 @@
-export function checkPassword(password: string, passwordConfirm: string): boolean {
-  if (password === passwordConfirm) {
+export function checkPassword(password: string, password_confirm: string): boolean {
+  if (password === password_confirm) {
     return /^[A-Za-z0-9\!@\$%_&+\*\(\)\-]{3,128}$/.test(password);
   } else {
     return false;
